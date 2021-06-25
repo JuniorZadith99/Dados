7a51f03523fa0f4d90363d790be3274a155ab16b
Junior Zadith Sierra Vasquez <jsierra_pr@sefin.gob.hn>
2021-06-25 10:35:16 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda = '../assets/img/dado1.png';
+  dadoDerecha = '../assets/img/dado4.png';
+
+  numero1: number | undefined;
+  numero2: number | undefined;
+
+  tirarDados(): void{
+    this.numero1 = Math.round (Math.random() * 5) + 1 ;
+    this.numero2 = Math.round (Math.random() * 5) + 1 ;
+    this.dadoIzquierda = '../assets/img/dado' + this.numero1 + '.png;'
+    this.dadoDerecha = '../assets/img/dado' + this.numero2 + '.png;'
+       } 
+
+       /* tirarDados(): void{
+        this.numero1 = Math.round (Math.random()) * 6;
+        this.numero2 = Math.random() * 6;
+        console.log(this.numero1);
+        console.log(this.numero2);
+      } */
 }
